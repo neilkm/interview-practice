5d103cfe11e917f29c3f3d00ef291d9bc8a08cdd
Neil Karkhanis <neil.karkhanis@evidentvascular.com>
2025-09-26 00:10:43 -0700

    need to add fail case, need to make test harness callable by top level main

--- code/LeetCode/src/bst.cpp
+++ code/LeetCode/src/bst.cpp
@@ -4,24 +4,24 @@
 namespace BinarySearch
 {
 	int Solution::searchInsert(std::vector<int> nums, int target)
-			{
-				int left = 0;
-				int right = nums.size();
-				int curr;
+	{
+		int left = 0;
+		int right = nums.size();
+		int curr;
 
-				if(nums[right - 1] < target) return right;
+		if(nums[right - 1] < target) return right;
 
-				while(left <= right) {
-					curr = (left + right) / 2;
-					if(nums[curr] == target) return curr;
+		while(left <= right) {
+			curr = (left + right) / 2;
+			if(nums[curr] == target) return curr;
 
-					if(nums[curr] > target) {
-							right = curr - 1;
-					} else {
-							left = curr + 1;
-					}
-				}
-				return left;
+			if(nums[curr] > target) {
+				right = curr - 1;
+			} else {
+				left = curr + 1;
 			}
+		}
+		return left;
+	}
 
 }//namespace
--- code/LeetCode/test/bst_unit_test.h
+++ code/LeetCode/test/bst_unit_test.h
@@ -19,6 +19,15 @@ namespace BinarySearch
 	std::vector<TestCase> bstTestCases = 
 	{
 		{"Odd nums existing target", {1,2,3,4,5}, 3, 2},
+		{"Even nums existing target", {1,2,3,4}, 3, 2},
+		{"Odd nums middle non existing target", {1,2,4,5,6}, 3, 2},
+		{"Even nums middle non existing target", {1,2,4,5}, 3, 2},
+		{"Odd nums high non existing target", {1,2,3,4,5}, 6, 5},
+		{"Even nums high non existing target", {1,2,4,5}, 6, 4},
+		{"Odd nums low non existing target", {1,2,3,4,5}, 0, 0},
+		{"Even nums low non existing target", {1,2,3,4}, 0, 0},
+		//{"Fail Case unordered input", {9,2,6,4}, 4, 0}, //explain why this passes
+		//come up with real fail case
 	};
 
 	void testBst() {
@@ -31,12 +40,15 @@ namespace BinarySearch
 		for(int i = 0; i < totalCaseCount; i++) {
 			auto testCase = bstTestCases[i];
 			std::cout << "case: " << testCase.testName << std::endl;
-
-			if(solution.searchInsert(testCase.nums,testCase.target) == testCase.expected) {
+			
+			int result = solution.searchInsert(testCase.nums,testCase.target);
+			if(result == testCase.expected) {
 				passed++;
 				std::cout << "PASSED" << std::endl;
 			} else {
 				std::cout << "FAILED" << std::endl;
+				std::cout << "Expected: " << testCase.expected << std::endl;
+				std::cout << "Received: " << result << std::endl; 
 			}
 		}
 		std::cout << passed << "/" << totalCaseCount << " cases passed." << std::endl;
